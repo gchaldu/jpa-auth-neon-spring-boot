21cfd61594d914e3ef9f2c4124e3c07a593d2938
Gabriel Chaldu <gchaldu@MacBook-Pro-de-Gabriel.local>
2025-06-09 09:42:59 -0300

    version con transacciones

--- src/main/java/com/youtics/spring_jpa_neon/product/service/ProductServiceImpl.java
+++ src/main/java/com/youtics/spring_jpa_neon/product/service/ProductServiceImpl.java
@@ -6,6 +6,7 @@ import com.youtics.spring_jpa_neon.product.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,21 +22,25 @@ public class ProductServiceImpl implements ProductService{
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Product> getAllProducts() {
         return repository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Product> getProductById(Long id) {
         return repository.findById(id);
     }
 
     @Override
+    @Transactional
     public Product createProduct(Product product) {
         return repository.save(product);
     }
 
     @Override
+    @Transactional
     public Product updateProduct(Long id, Product product) {
         return repository.findById(id)
                 .map(existing -> {
@@ -49,6 +54,7 @@ public class ProductServiceImpl implements ProductService{
 
 
     @Override
+    @Transactional
     public void deleteProduct(Long id) {
         Product product = repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Producto no encontrado con ID: " + id));
